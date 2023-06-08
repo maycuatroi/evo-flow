3f81e7dd4f44d8a5a8c18b949c9db946f38b30c4
maycuatroi <maycuatroi@users.noreply.github.com>
2023-06-08 08:28:31 +0000

    ✅ Ready to clone and code.

--- evo_flow/cli.py
+++ evo_flow/cli.py
@@ -1,4 +1,4 @@
-"""CLI interface for project_name project.
+"""CLI interface for evo_flow project.
 
 Be creative! do whatever you want!
 
@@ -12,7 +12,7 @@ Be creative! do whatever you want!
 def main():  # pragma: no cover
     """
     The main function executes on commands:
-    `python -m project_name` and `$ project_name `.
+    `python -m evo_flow` and `$ evo_flow `.
 
     This is your program's entry point.
 
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,5 @@
-from project_name.base import NAME
+from evo_flow.base import NAME
 
 
 def test_base():
-    assert NAME == "project_name"
+    assert NAME == "evo_flow"
